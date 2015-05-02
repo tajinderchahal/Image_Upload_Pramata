82839380193e07e8d76c4cc48ea5b0e305cdf828
Tajinder <tajinder.chahal14@gmail.com>
2015-05-02 19:39:02 +0530

    procfile included

--- settings.py
+++ settings.py
@@ -97,7 +97,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
